b1d75f50fc3bbb1d247db2c7b41adb61016379da
Lucas Isla <lisla@purdue.edu>
2018-04-06 18:25:38 -0500

    Tweak vader_sentiment to analyze sentiment of lyrics (not song name).

--- scrape_lyrics.py
+++ scrape_lyrics.py
@@ -36,15 +36,15 @@ def scrape_lyrics(file_name):
         lyrics_div = soup.find_all('div', class_="lyrics")
         lyrics_p = lyrics_div[0].find('p')
         lyrics_text = lyrics_p.text
-        # Remove [Verse], etc. labels
+        # Remove [Verse], '\n', etc.
         lyrics_text = re.sub(r'(?is)\[.*?\]\n', '', lyrics_text)
-
+        lyrics_text = re.sub("\n", ' ', lyrics_text)
         # Writes the lyrics to a '.txt' file.
         with open(file_name, 'w') as outfile:
             outfile.write(lyrics_text.encode('utf-8'))
 
         # Extract song_name from url
-        song_name = url[19:]
+        song_name = url[19:].encode('ascii')
         # Create new dictionary entry for song lyrics
         lyrics[song_name] = lyrics_text
 
